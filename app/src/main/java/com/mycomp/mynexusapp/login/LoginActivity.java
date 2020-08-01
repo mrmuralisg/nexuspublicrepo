9b9fc3ca9fdea284f01ee7e5095da44f58c77023
mrmuralisg <mr.muralisg@gmail.com>
2020-08-01 23:50:22 +0800

    first commit

---
+++ app/src/main/java/com/mycomp/mynexusapp/login/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.mycomp.mynexusapp.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mycomp.mynexusapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //LoginRequest req=null;
+    }
+}
